0e54012ff902e8f199abdec1dec72d4e00f2ebcf
seocxwoo <seocxwoo@gmail.com>
2025-06-19 16:12:23 +0900

    Fix: 에러 효과 수정

--- Assets/seocxwoo/Scripts/Robot/AlertBillboard.cs
+++ Assets/seocxwoo/Scripts/Robot/AlertBillboard.cs
@@ -2,12 +2,16 @@ using UnityEngine;
 
 public class AlertBillboard : MonoBehaviour
 {
+    [SerializeField] private Camera targetCamera;
+
     void Update()
     {
+        if (targetCamera == null) return;
+
         // 카메라를 향하도록 회전
-        transform.LookAt(Camera.main.transform);
+        transform.LookAt(targetCamera.transform);
 
         // 필요 시 회전을 뒤집어 Z축을 앞면으로 만들기
-        transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
+        transform.rotation = Quaternion.LookRotation(targetCamera.transform.forward);
     }
 }
